56c701e1837dd53f97f2565f9d10adfd8320f7ec
Fred <fred.bahena@gmail.com>
2021-07-06 19:17:17 -0500

    Prueba contacto php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,40 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+  $errors = '';
+  $myemail = 'mdm.digitalagency@gmail.com'; 
+  if(empty($_POST['name'])  || 
+    empty($_POST['email']) ||
+    empty($_POST['subject']) || 
+    empty($_POST['message']))
+  {
+      $errors .= "\n Error: Todos los campos son requeridos";
   }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $name = $_POST['name']; 
+  $email_address = $_POST['email'];
+  $subject = $_POST['subject']; 
+  $message = $_POST['message']; 
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  if (!preg_match(
+  "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+  $email_address))
+  {
+      $errors .= "\n Error: Correo electrónico no válido";
+  }
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  if( empty($errors))
+  {
+    $to = $myemail; 
+    $email_subject = "MDM - $subject :: $name";
+    $email_body = "You have received a new message. ".
+    " Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+    
+    $headers = "From: $myemail\n"; 
+    $headers .= "Reply-To: $email_address";
+    
+    mail($to,$email_subject,$email_body,$headers);
+    
+  } 
 
-  echo $contact->send();
+    
 ?>
