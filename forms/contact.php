7e666177e34050f3b4ae7e58ca59c7957cb21156
Fred <fred.bahena@gmail.com>
2021-07-06 20:42:49 -0500

    favicon, sendmail, textos

--- forms/contact.php
+++ forms/contact.php
@@ -1,40 +1,48 @@
 <?php
 
-  $errors = '';
-  $myemail = 'mdm.digitalagency@gmail.com'; 
+  $errors = 0;
+  $msg = '';
+
+  $from = 'mdm.digitalagency@gmail.com'; 
+
   if(empty($_POST['name'])  || 
     empty($_POST['email']) ||
     empty($_POST['subject']) || 
     empty($_POST['message']))
   {
-      $errors .= "\n Error: Todos los campos son requeridos";
+    $msg = 'Error: Todos los campos son requeridos'
+    die( $msg );
+    $errors += 1;
   }
 
   $name = $_POST['name']; 
-  $email_address = $_POST['email'];
+  $to = $_POST['email'];
   $subject = $_POST['subject']; 
   $message = $_POST['message']; 
 
-  if (!preg_match(
-  "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-  $email_address))
-  {
-      $errors .= "\n Error: Correo electrónico no válido";
+  if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $to)) {
+    $msg = 'Error: Correo electrónico no válido'
+    die( $msg );
+    $errors += 1
   }
 
-  if( empty($errors))
-  {
-    $to = $myemail; 
+  if( $errors == 0 ) {
+    
     $email_subject = "MDM - $subject :: $name";
-    $email_body = "You have received a new message. ".
-    " Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+    $email_message = "Recibiste un nuevo mensaje. Aquí los detalles:".
+    "\n Nombre: $name \n Email: $to \n Mensaje: \n $message"; 
     
-    $headers = "From: $myemail\n"; 
-    $headers .= "Reply-To: $email_address";
-    
-    mail($to,$email_subject,$email_body,$headers);
-    
-  } 
+    $headers = "From: $from"; 
+        
+    mail($to,$email_subject,$email_message,$headers);
 
+    $msg = 'OK';
+    echo $msg;
+    
+  } else {
+
+    echo $msg;
+
+  }
     
 ?>
